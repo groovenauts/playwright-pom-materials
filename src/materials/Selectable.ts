2f4d87c9fc67b98a271a4a66bc8b58051a8344b1
akm <akm2000@gmail.com>
2023-01-01 17:18:05 +0900

    :sparkles: Add Selectable

---
+++ src/materials/Selectable.ts
@@ -0,0 +1,70 @@
+import {Locator} from 'playwright-core';
+
+import {
+  LocatorPredicate,
+  reverseLocatorPredicate,
+  pwRetry,
+  uiRetry,
+  hasClassFunc,
+  RetryOptions,
+} from '../helpers/playwright';
+
+import {Clickable, ClickableOptions} from './Clickable';
+
+export const hasSelectedClass = hasClassFunc('selected');
+
+export type SelectableOptions = ClickableOptions & {
+  selectedPredicate?: LocatorPredicate;
+};
+
+export class Selectable extends Clickable {
+  protected selectedPredicate: LocatorPredicate;
+
+  constructor(locator: Locator, options?: SelectableOptions) {
+    super(locator, options);
+    this.selectedPredicate = options?.selectedPredicate || hasSelectedClass;
+  }
+
+  isSelected(): Promise<boolean> {
+    return this.selectedPredicate(this.locator);
+  }
+
+  set(value: boolean, options?: RetryOptions): Promise<void> {
+    return value ? this.select(options) : this.deselect(options);
+  }
+
+  async shouldBeSelected(): Promise<void> {
+    await pwRetry(this.locator, this.selectedPredicate);
+  }
+
+  async shouldNotBeSelected(): Promise<void> {
+    await pwRetry(
+      this.locator,
+      reverseLocatorPredicate(this.selectedPredicate)
+    );
+  }
+
+  async select(options?: RetryOptions): Promise<void> {
+    await uiRetry(
+      this.locator,
+      async () => {
+        if (await this.isSelected()) return true;
+        await this.click();
+        return false;
+      },
+      options
+    );
+  }
+
+  async deselect(options?: RetryOptions): Promise<void> {
+    await uiRetry(
+      this.locator,
+      async () => {
+        if (!(await this.isSelected())) return true;
+        await this.click();
+        return false;
+      },
+      options
+    );
+  }
+}
