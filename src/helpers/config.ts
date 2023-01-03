fa9aeaf501daca15ee062b817e08471ed83ac4fd
akm <akm2000@gmail.com>
2023-01-03 17:18:27 +0900

    :sparkles: Define Config type to replace config by user

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,3 +1,11 @@
-export const config = {
+export type Config = {
+  readonly timeoutUnit: number;
+};
+
+export const setConfig = (newConfig: Config): void => {
+  config = newConfig;
+};
+
+export let config = {
   timeoutUnit: 1_000,
 };
