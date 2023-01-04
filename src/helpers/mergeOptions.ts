a92286e24022f6c78c7f6a951f0b9a7824d04070
akm <akm2000@gmail.com>
2023-01-04 10:12:03 +0900

    :sparkles: Don't use spread operator

--- src/helpers/mergeOptions.ts
+++ src/helpers/mergeOptions.ts
@@ -2,22 +2,10 @@ export const mergeOptions = <T>(defaultOptions: T, options?: Partial<T>): T => {
   if (!options) {
     return defaultOptions;
   } else {
-    return {
-      ...defaultOptions,
-      ...filterUndefined(options),
-    };
-  }
-};
-
-/**
- * Options must be a type of object which allows any undefined value
- */
-const filterUndefined = <Options>(obj: Options): Options => {
-  const result = {} as Options;
-  for (const key in obj) {
-    if (obj[key] !== undefined) {
-      result[key] = obj[key];
+    const result = {} as Partial<T>;
+    for (const key in defaultOptions) {
+      result[key] = options[key] ?? defaultOptions[key];
     }
+    return result as T;
   }
-  return result;
 };
