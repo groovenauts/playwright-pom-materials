bb744fda62e5f27e953b3ac78018ed8d01c6043d
akm <akm2000@gmail.com>
2023-12-28 13:20:13 +0900

    :sparkles: Locator インタフェースをimplements するようにDisplayable に定義を追加
    メソッドは未実装

--- src/materials/Displayable.ts
+++ src/materials/Displayable.ts
@@ -1,6 +1,6 @@
 import {Locator} from '@playwright/test';
 
-export class Displayable {
+export class Displayable implements Locator {
   constructor(readonly _locator: Locator) {}
 
   isVisible(): Promise<boolean> {
