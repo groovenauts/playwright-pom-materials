6949e1a7487bac7e5426fe1649d4c21ffc1eab88
akm <akm2000@gmail.com>
2024-01-01 17:04:42 +0900

    :sparkles: Define extended expect for Displayable

---
+++ src/assertions/expect.ts
@@ -0,0 +1,36 @@
+import {expect as origExpect} from '@playwright/test';
+import type {Locator} from '@playwright/test';
+import {Displayable} from '../materials/Displayable';
+
+type ExpectReturnType = ReturnType<typeof origExpect>;
+type SoftType = typeof origExpect.soft;
+type ConfigureType = typeof origExpect.configure;
+type PollType = typeof origExpect.poll;
+
+export type ExpectType = {
+  (
+    x: Locator | Displayable,
+    messageOrOptions?: string | {message?: string}
+  ): ExpectReturnType;
+  soft: SoftType;
+  configure: ConfigureType;
+  poll: PollType;
+};
+
+// playwright の expect を拡張して Displayable 以下のクラスを受け付けるようにします。
+// ただし以下の機能を使うことはできません。
+// - expect.extend
+export const expect = ((): ExpectType => {
+  const body = (
+    x: Locator | Displayable,
+    messageOrOptions?: string | {message?: string}
+  ): ExpectReturnType => {
+    const locator = x instanceof Displayable ? x.locator : x;
+    return origExpect(locator as unknown, messageOrOptions);
+  };
+  return Object.assign(body, {
+    soft: origExpect.soft.bind(body),
+    configure: origExpect.configure.bind(body),
+    poll: origExpect.poll.bind(body),
+  });
+})();
