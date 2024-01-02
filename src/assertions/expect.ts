3c11fc3c97539b05e6e0f6eb77b33e1bf4273903
akm <akm2000@gmail.com>
2024-01-02 11:00:15 +0900

    :sparkles: :rotating_light: expect の引数と戻り値の型パラメーターTを連携
    しかし、ビルドするとエラーに。
    
    $ npm run build
    
    > playwright-pom-materials@0.3.0 build
    > run-s compile lint
    
    > playwright-pom-materials@0.3.0 compile
    > tsc
    
    src/assertions/expect.ts:41:14 - error TS4023: Exported variable 'expect' has or is using name 'APIResponseAssertions' from external module "/Users/takeshi/groovenauts/playwright-pom-materials/node_modules/playwright/types/test" but cannot be named.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    src/assertions/expect.ts:41:14 - error TS4023: Exported variable 'expect' has or is using name 'GenericAssertions' from external module "/Users/takeshi/groovenauts/playwright-pom-materials/node_modules/playwright/types/test" but cannot be named.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    src/assertions/expect.ts:41:14 - error TS4023: Exported variable 'expect' has or is using name 'LocatorAssertions' from external module "/Users/takeshi/groovenauts/playwright-pom-materials/node_modules/playwright/types/test" but cannot be named.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    src/assertions/expect.ts:41:14 - error TS4023: Exported variable 'expect' has or is using name 'PageAssertions' from external module "/Users/takeshi/groovenauts/playwright-pom-materials/node_modules/playwright/types/test" but cannot be named.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    src/assertions/expect.ts:41:14 - error TS4023: Exported variable 'expect' has or is using name 'SnapshotAssertions' from external module "/Users/takeshi/groovenauts/playwright-pom-materials/node_modules/playwright/types/test" but cannot be named.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    src/assertions/expect.ts:41:14 - error TS7056: The inferred type of this node exceeds the maximum length the compiler will serialize. An explicit type annotation is needed.
    
    41 export const expect = expectBody;
                    ~~~~~~
    
    Found 6 errors in the same file, starting at: src/assertions/expect.ts:41
    
    ERROR: "compile" exited with 1.

--- src/assertions/expect.ts
+++ src/assertions/expect.ts
@@ -1,17 +1,17 @@
-import {expect as origExpect} from '@playwright/test';
+import {Locator, expect as origExpect} from '@playwright/test';
 import {Displayable} from '../materials/Displayable';
 
-type ExpectReturnType = ReturnType<typeof origExpect>;
+type ExpectReturnType<T> = ReturnType<typeof origExpect<T>>;
 type SoftType = typeof origExpect.soft;
 type ConfigureType = typeof origExpect.configure;
 type PollType = typeof origExpect.poll;
 type ExtendType = typeof origExpect.extend;
 
 export type ExpectType = {
-  (
-    x: unknown,
+  <T>(
+    x: T,
     messageOrOptions?: string | {message?: string}
-  ): ExpectReturnType;
+  ): ExpectReturnType<T>;
 
   // These methods are experimental
   // See https://playwright.dev/docs/test-assertions#negating-matchers for more detail
@@ -21,20 +21,21 @@ export type ExpectType = {
   extend: ExtendType;
 };
 
-export const expect = ((): ExpectType => {
-  const body = (
-    x: unknown,
-    messageOrOptions?: string | {message?: string}
-  ): ExpectReturnType => {
-    return origExpect(
-      x instanceof Displayable ? x._locator : x,
-      messageOrOptions
-    );
-  };
-  return Object.assign(body, {
-    soft: origExpect.soft.bind(body),
-    configure: origExpect.configure.bind(body),
-    poll: origExpect.poll.bind(body),
-    extend: origExpect.extend.bind(body),
-  });
-})();
+const expectBody = <T>(
+  x: T,
+  messageOrOptions?: string | {message?: string}
+): ExpectReturnType<T | Locator> => {
+  return origExpect(
+    x instanceof Displayable ? x._locator : x,
+    messageOrOptions
+  );
+};
+
+// export const expect = Object.assign(expectBody, {
+//   soft: origExpect.soft.bind(expectBody),
+//   configure: origExpect.configure.bind(expectBody),
+//   poll: origExpect.poll.bind(expectBody),
+//   extend: origExpect.extend.bind(expectBody),
+// });
+
+export const expect = expectBody;
